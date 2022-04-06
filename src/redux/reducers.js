15dd79121c5ecf321d2cc505a61ff5786f484996
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-04-06 02:31:53 +0100

    Add events to reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,8 @@
 import {    combineReducers    } from "redux";
     import products from "./slices/productsSlice";
     import brands from "./slices/brandsSlice";
+    import events from "./slices/eventSlice"
     const reducers = combineReducers({
-    products,brands
+    products,brands, events
     });
     export default reducers;
