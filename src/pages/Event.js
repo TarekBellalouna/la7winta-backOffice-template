924b3b444aeb98e732033d8cd1ea0e838184e9b6
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-04-06 02:31:24 +0100

    Page event

---
+++ src/pages/Event.js
@@ -0,0 +1,95 @@
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteEvent, selectEvent } from '../redux/slices/eventSlice';
+import {queryApi} from '../utils/queryApi'
+// material
+import { Container, Stack, Typography } from '@mui/material';
+// components
+import Page from '../components/Page';
+import {
+  ProductSort,
+  ProductList,
+  ProductCartWidget,
+  ProductFilterSidebar
+} from '../sections/@dashboard/products';
+//
+import PRODUCTS from '../_mocks_/products';
+import EventList from 'src/sections/@dashboard/events/EventList';
+
+// ----------------------------------------------------------------------
+
+export default function EcommerceShop() {
+  const [openFilter, setOpenFilter] = useState(false);
+
+  const dispatch = useDispatch();
+ 
+  const [events, err] = useSelector(selectEvent);
+console.log({events})
+  const deleteProductFunc = async (id) => {
+    const [, err] = await queryApi('products/delete-product/', {productId:id}, 'POST');
+    if (err) {
+      console.log(err);
+    } else dispatch(deleteProduct(id));
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      gender: '',
+      category: '',
+      colors: '',
+      priceRange: '',
+      rating: ''
+    },
+    onSubmit: () => {
+      setOpenFilter(false);
+    }
+  });
+
+  const { resetForm, handleSubmit } = formik;
+
+  const handleOpenFilter = () => {
+    setOpenFilter(true);
+  };
+
+  const handleCloseFilter = () => {
+    setOpenFilter(false);
+  };
+
+  const handleResetFilter = () => {
+    handleSubmit();
+    resetForm();
+  };
+
+  return (
+    <Page title="Dashboard: Products | Minimal-UI">
+      <Container>
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Event
+        </Typography>
+
+        <Stack
+          direction="row"
+          flexWrap="wrap-reverse"
+          alignItems="center"
+          justifyContent="flex-end"
+          sx={{ mb: 5 }}
+        >
+          <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
+            <ProductFilterSidebar
+              formik={formik}
+              isOpenFilter={openFilter}
+              onResetFilter={handleResetFilter}
+              onOpenFilter={handleOpenFilter}
+              onCloseFilter={handleCloseFilter}
+            />
+            <ProductSort />
+          </Stack>
+        </Stack>
+
+        <EventList events={events} deleteProduct={deleteProductFunc} />
+        <ProductCartWidget />
+      </Container>
+    </Page>
+  );
+}
