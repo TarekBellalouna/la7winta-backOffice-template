8c4daa34173fafee62dea33ee0e503de2ac2e661
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-04-23 16:57:00 +0100

    correct errors

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,8 +1,12 @@
 import {    combineReducers    } from "redux";
     import products from "./slices/productsSlice";
     import brands from "./slices/brandsSlice";
-    import events from "./slices/eventSlice"
+    import events from "./slices/eventSlice";
+    import users from "./slices/userSlice"
     const reducers = combineReducers({
-    products,brands, events
+   
+  
+    products,brands, events,users
+ 
     });
     export default reducers;
