56e1ebff8e38fbc9132b9a7ba67fe2918dafa37b
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-04-26 23:33:46 +0100

    event slice

--- src/redux/slices/eventSlice.js
+++ src/redux/slices/eventSlice.js
@@ -1,7 +1,8 @@
-import { createSlice} from "@reduxjs/toolkit";
-import axios from "axios";
-import { queryApi} from "../../utils/queryApi";
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { queryApi } from '../../utils/queryApi';
 let initialState = {
+<<<<<<< Updated upstream
 events: [],
 selectedEvent: {},
 deletedEvent:"",
@@ -31,38 +32,85 @@ setErrors(state, action) {
 state.errors = action.payload;
 },
 },
+=======
+  events: [],
+  eventsCount: null,
+  selectedEvent: {},
+  deletedEvent: '',
+  errors: ''
+};
 
+const eventSlice = createSlice({
+  name: 'events',
+  initialState,
+  reducers: {
+    populateEvent(state, action) {
+      state.events = action.payload;
+    },
+    selectEvent(state, action) {
+      state.selectedEvent = action.payload;
+    },
+    unselectEvent(state) {
+      state.selectedProduct = null;
+    },
+    deleteEvent: (state, action) => {
+      state.deletedEvent = action.payload;
+    },
+    getEventCount(state, action) {
+      state.eventsCount = action.payload;
+    },
+>>>>>>> Stashed changes
+
+    setErrors(state, action) {
+      state.errors = action.payload;
+    }
+  }
 });
 export const fetchEvents = () => async (dispatch) => {
-const [res, error] = await queryApi("event/all?searchValue");
-console.log(res)
-if (error) {
-dispatch(setErrors(error));
-} else {
-    
-dispatch(populateEvent(res));
-}
+  const [res, error] = await queryApi('event/all?searchValue');
+  console.log(res);
+  if (error) {
+    dispatch(setErrors(error));
+  } else {
+    dispatch(populateEvent(res));
+  }
 };
+<<<<<<< Updated upstream
+=======
+export const fetchEventsCount = (from) => async (dispatch) => {
+  const [res, error] = await queryApi(`event/getEventsByTime?from=${from}`);
+  console.log(res);
+  if (error) {
+    dispatch(setErrors(error));
+  } else {
+    dispatch(getEventCount(res));
+  }
+};
+>>>>>>> Stashed changes
 export const deleteEvents = (id) => async (dispatch) => {
-    const res = await axios.delete(`http://localhost:5000/event/delete/${id}`);
-    console.log(res)
-    
-        
-    dispatch(deleteEvent(res));
-    }
-    
-export const selectEvent = (state) => {
-return [state.events.events, state.events.errors];
-};
-export const deleteEventfunction = (state) => {
-    return [state.events.deletedEvent, state.events.errors];
+  const res = await axios.delete(`http://localhost:5000/event/delete/${id}`);
+  console.log(res);
 
-    
-    };
+  dispatch(deleteEvent(res));
+};
+
+export const selectEvent = (state) => {
+  return [state.events.events, state.events.errors];
+};
+<<<<<<< Updated upstream
+=======
+export const selectEventCount = (state) => {
+  return [state.events.eventsCount, state.events.errors];
+};
+>>>>>>> Stashed changes
+export const deleteEventfunction = (state) => {
+  return [state.events.deletedEvent, state.events.errors];
+};
 export const selectSelectedProduct = (state) => {
-return state.products.selectedProduct;
+  return state.products.selectedProduct;
 };
 export const {
+<<<<<<< Updated upstream
 populateEvent,
 selectProduct,
 unselectProduct,
@@ -70,5 +118,15 @@ setErrors,
 deleteEvent,
 updateProduct,
 addProduct,
+=======
+  populateEvent,
+  getEventCount,
+  selectProduct,
+  unselectProduct,
+  setErrors,
+  deleteEvent,
+  updateProduct,
+  addProduct
+>>>>>>> Stashed changes
 } = eventSlice.actions;
 export default eventSlice.reducer;
