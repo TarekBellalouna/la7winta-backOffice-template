381e4baa0f8783b3307f93e0457a29b70f9d3a73
akramthabet <81181377+akramthabet@users.noreply.github.com>
2022-04-26 23:28:23 +0100

    add reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,9 @@
 import {    combineReducers    } from "redux";
     import products from "./slices/productsSlice";
     import brands from "./slices/brandsSlice";
+    import comments from "./slices/commentSlice";
+    import ratings from "./slices/ratingSlice";
     const reducers = combineReducers({
-    products,brands
+    products,brands,comments,ratings
     });
     export default reducers;
